110e662cdf064bb70a6da0e15316684073c9213c
Kirill Sysoev <kirill.sysoev@gmail.com>
2020-02-02 13:40:01 +0300

    update

--- reseller/portal/models/rs_mask_history.ts
+++ reseller/portal/models/rs_mask_history.ts
@@ -27,8 +27,6 @@ const Model: ModelType = {
     *demask(_, { put, select }) {
       console.log('Demask attempt');
       const state = yield select();
-      console.log('Demask attempt state');
-      console.log(state);
       const account_id = state.rs_mask_history.children.account_id
         ? state.rs_mask_history.children.account_id
         : state.kazoo_login.data.account_id;
@@ -47,19 +45,22 @@ const Model: ModelType = {
         type: 'kazoo_user/refresh',
         payload: { account_id, owner_id },
       });
+      yield window.g_app._store.dispatch({
+        type: 'rs_child_account/refresh',
+        payload: { account_id: state.rs_mask_history.account_id },
+      });
       yield put({
         type: 'levelup',
       });
     },
     *mask({ payload }, { put }) {
-      console.log('Model mask payload: ', payload);
       yield window.g_app._store.dispatch({
         type: 'lb_account/refresh',
         payload: { account_id: payload.account_id },
       });
       yield put({
         type: 'level_down',
-        payload: { payload },
+        payload: payload,
       });
     },
   },
@@ -69,7 +70,6 @@ const Model: ModelType = {
       return { ...payload, children: state };
     },
     levelup(state) {
-      console.log('LevelUp attempt');
       return { ...state.children };
     },
   },
