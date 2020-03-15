151b01aca6c8c1922fb214208aec1bb81f416a54
Kirill Sysoev <kirill.sysoev@gmail.com>
2020-03-15 20:10:29 +0300

    update

--- core/components/DevicesList/DeviceAssignTo.js
+++ core/components/DevicesList/DeviceAssignTo.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { connect } from 'dva';
-
+import * as _ from 'lodash';
 import { formatMessage } from 'umi-plugin-react/locale';
 import { EditOutlined } from '@ant-design/icons';
 import { Button, Select, Modal } from 'antd';
@@ -12,6 +12,7 @@ import { kzDevice } from '@/pages/onnet-portal/core/services/kazoo';
 const DeviceAssignTo = props => {
   const [buttonVisible, setButtonVisible] = useState(false);
   const [selectedId, setSelectedId] = useState('');
+  const [dataForSelect, setDataForSelect] = useState([]);
   const [currentValue, setCurrentValue] = useState(
     formatMessage({
       id: 'core.No_owner',
@@ -31,7 +32,7 @@ const DeviceAssignTo = props => {
     try {
       const ownerId = full_devices[device_id].data.owner_id;
       if (ownerId) {
-        return full_users[ownerId].data.username;
+        return `${full_users[ownerId].data.username} (${full_users[ownerId].data.first_name} ${full_users[ownerId].data.last_name})`;
       } else {
         return formatMessage({ id: 'core.No_owner', defaultMessage: '-No owner-' });
       }
@@ -42,6 +43,7 @@ const DeviceAssignTo = props => {
 
   useEffect(() => {
     if (account.data && full_devices[device_id]) {
+      setDataForSelect(brief_users.data);
       const currVal = currentDocValue();
       setCurrentValue(currVal);
       setModalTitle(
@@ -52,11 +54,6 @@ const DeviceAssignTo = props => {
       );
     }
   }, [account, full_devices, full_users]);
-
-  const onSelect = event => {
-    console.log('onSelect event: ', event);
-    setSelectedId(event);
-  };
 
   const onModalConfirm = props => {
     if (selectedId === "no_owner_key") {
@@ -94,10 +91,31 @@ const DeviceAssignTo = props => {
     setButtonVisible(false);
   };
 
+  const onSelect = event => {
+    setSelectedId(event);
+    setDataForSelect(brief_users.data);
+  };
+
   const onModalCancel = () => {
     setCurrentValue(currentDocValue());
+    setDataForSelect(brief_users.data);
     setButtonVisible(false);
   };
+
+  const selectSearch = val => {
+    const searchRes = _.filter(brief_users.data, (o) => _.includes(_.toString(Object.values(o)), val));
+    setDataForSelect(searchRes);
+  };
+
+  const options = 
+            dataForSelect.map(user => (
+              <Select.Option value={user.id} key={user.id}>
+		{user.username} ({user.first_name} {user.last_name})
+              </Select.Option>
+            )).concat(
+            <Select.Option value="no_owner_key" key="no_owner_key">
+              {formatMessage({ id: 'core.No_owner', defaultMessage: '-No owner-' })}
+            </Select.Option>);
 
   return (
     <>
@@ -113,17 +131,14 @@ const DeviceAssignTo = props => {
       >
         <div style={{ textAlign: 'center' }}>
           <Select
-            style={{ width: '50%' }}
+            style={{ width: '65%' }}
             onChange={onSelect}
             showSearch
+	    filterOption={false}
+	    onSearch={selectSearch}
             defaultValue={currentValue}
           >
-            {brief_users.data.map(user => (
-              <Select.Option value={user.id} key={user.id}>{user.username}</Select.Option>
-            ))}
-            <Select.Option value="no_owner_key" key="no_owner_key">
-              {formatMessage({ id: 'core.No_owner', defaultMessage: '-No owner-' })}
-            </Select.Option>
+	    {options}
           </Select>
         </div>
       </Modal>
