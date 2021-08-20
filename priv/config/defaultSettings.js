2c703d068eb06738c35110040dca9ec3d4bc803a
iam <iam@pop-os.localdomain>
2021-08-20 12:33:05 +0300

    update

---
+++ priv/config/defaultSettings.js
@@ -0,0 +1,17 @@
+const Settings = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'OnNet',
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+  crossbarUrlV2: 'http://kz533.onnet.su:8000/v2',
+  blackholeUrl: 'ws://kz533.onnet.su:5555/',
+};
+export default Settings;
