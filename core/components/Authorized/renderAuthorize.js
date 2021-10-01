49e779ddd935b047005f5b76fcc84e727ee0689c
iam <iam@ivan-room.lan>
2021-10-01 18:20:37 +0300

    update

---
+++ core/components/Authorized/renderAuthorize.js
@@ -0,0 +1,22 @@
+const CURRENT;
+
+const renderAuthorize =
+  (Authorized) =>
+  (currentAuthority) => {
+    if (currentAuthority) {
+      if (typeof currentAuthority === 'function') {
+        CURRENT = currentAuthority();
+      }
+      if (
+        Object.prototype.toString.call(currentAuthority) === '[object String]' ||
+        Array.isArray(currentAuthority)
+      ) {
+        CURRENT = currentAuthority;
+      }
+    } else {
+      CURRENT = 'NULL';
+    }
+    return Authorized;
+  };
+
+export default (Authorized) => renderAuthorize(Authorized);
