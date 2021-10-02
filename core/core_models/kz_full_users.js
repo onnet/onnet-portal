35595adfe4c2b9814a9627faa00f3f9f3a3418a1
iam <iam@ivan-room.lan>
2021-10-02 18:21:05 +0300

    from ts

--- core/core_models/kz_full_users.js
+++ core/core_models/kz_full_users.js
@@ -1,19 +1,6 @@
-import { Effect, Reducer } from 'umi';
 import { getUser } from '../services/kazoo';
 
-export interface ModelType {
-  namespace: string;
-  state: {};
-  effects: {
-    refreshAccountState: Effect;
-  };
-  reducers: {
-    update: Reducer<{}>;
-    flush: Reducer<{}>;
-  };
-}
-
-const Model: ModelType = {
+const Model = {
   namespace: 'kz_full_users',
 
   state: {},
