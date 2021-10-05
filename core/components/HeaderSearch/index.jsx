4154d4c526c475740bf5a607f17056526046b2c8
iam <iam@ivan-room.lan>
2021-10-05 10:58:14 +0300

    from tsx

--- core/components/HeaderSearch/index.jsx
+++ core/components/HeaderSearch/index.jsx
@@ -1,32 +1,18 @@
 import { SearchOutlined } from '@ant-design/icons';
 import { AutoComplete, Input } from 'antd';
 import useMergeValue from 'use-merge-value';
-import { AutoCompleteProps } from 'antd/es/auto-complete';
 import React, { useRef } from 'react';
 
 import classNames from 'classnames';
 import styles from './index.less';
 
-export interface HeaderSearchProps {
-  onSearch?: (value?: string) => void;
-  onChange?: (value?: string) => void;
-  onVisibleChange?: (b: boolean) => void;
-  className?: string;
-  placeholder?: string;
-  options: AutoCompleteProps['options'];
-  defaultOpen?: boolean;
-  open?: boolean;
-  defaultValue?: string;
-  value?: string;
-}
-
-const HeaderSearch: React.FC<HeaderSearchProps> = (props) => {
+const HeaderSearch = (props) => {
   const { className, defaultValue, onVisibleChange, placeholder, open, defaultOpen, ...restProps } =
     props;
 
-  const inputRef = useRef<Input | null>(null);
+  const inputRef = useRef(null);
 
-  const [value, setValue] = useMergeValue<string | undefined>(defaultValue, {
+  const [value, setValue] = useMergeValue(defaultValue, {
     value: props.value,
     onChange: props.onChange,
   });
