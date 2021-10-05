f3d37f27c47d718203670dd63ae601bdc4449619
iam <iam@ivan-room.lan>
2021-10-05 10:41:30 +0300

    from ts

--- reseller/portal/models/mask_history.js
+++ reseller/portal/models/mask_history.js
@@ -1,25 +1,6 @@
 import { getDvaApp } from 'umi';
-import { AnyAction, Reducer } from 'redux';
-import { EffectsCommandMap } from 'dva';
 
-export type Effect = (
-  action: AnyAction,
-  effects: EffectsCommandMap & { select: <T>(func: (state: {}) => T) => T },
-) => void;
-
-export interface ModelType {
-  namespace: string;
-  state: {};
-  effects: {
-    refreshAccountState: Effect;
-  };
-  reducers: {
-    update: Reducer<{}>;
-    flush: Reducer<{}>;
-  };
-}
-
-const Model: ModelType = {
+const Model = {
   namespace: 'mask_history',
 
   state: {},
