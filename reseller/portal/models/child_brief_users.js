c05e655c52275a15d94f637e248943a997b670ca
iam <iam@ivan-room.lan>
2021-10-05 10:43:50 +0300

    from ts

--- reseller/portal/models/child_brief_users.js
+++ reseller/portal/models/child_brief_users.js
@@ -1,26 +1,7 @@
 import { getDvaApp } from 'umi';
-import { AnyAction, Reducer } from 'redux';
-import { EffectsCommandMap } from 'dva';
 import { getUsers } from '@/pages/onnet-portal/core/services/kazoo';
 
-export type Effect = (
-  action: AnyAction,
-  effects: EffectsCommandMap & { select: <T>(func: (state: {}) => T) => T },
-) => void;
-
-export interface ModelType {
-  namespace: string;
-  state: {};
-  effects: {
-    refreshAccountState: Effect;
-  };
-  reducers: {
-    update: Reducer<{}>;
-    flush: Reducer<{}>;
-  };
-}
-
-const Model: ModelType = {
+const Model = {
   namespace: 'child_brief_users',
 
   state: {},
--- reseller/portal/models/child_full_users.js
+++ reseller/portal/models/child_full_users.js
@@ -1,25 +1,6 @@
-import { AnyAction, Reducer } from 'redux';
-import { EffectsCommandMap } from 'dva';
 import { getUser } from '@/pages/onnet-portal/core/services/kazoo';
 
-export type Effect = (
-  action: AnyAction,
-  effects: EffectsCommandMap & { select: <T>(func: (state: {}) => T) => T },
-) => void;
-
-export interface ModelType {
-  namespace: string;
-  state: {};
-  effects: {
-    refreshAccountState: Effect;
-  };
-  reducers: {
-    update: Reducer<{}>;
-    flush: Reducer<{}>;
-  };
-}
-
-const Model: ModelType = {
+const Model = {
   namespace: 'child_full_users',
 
   state: {},
